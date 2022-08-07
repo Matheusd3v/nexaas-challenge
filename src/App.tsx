934cd6cb464b936d94a6a012a1d9efba889cc38d
Matheus <matheus.g.almeida21@gmail.com>
2022-08-07 10:58:24 -0300

    refac: change default size of input atom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
+import AsideBar from "./components/organisms/asideBar";
 import NavBar from "./components/organisms/navBar";
 
 function App() {
   return (
     <>
       <NavBar/>
-    </>
+      <AsideBar/>
+      </>
   );
 }
 
--- src/components/atoms/input/index.tsx
+++ src/components/atoms/input/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { InputNavBar } from './style'
 
-const Input = ({ height=40, width=600, placeholder='' }) => {
+const Input = ({ height=40, width=700, placeholder='' }) => {
   return (
     <InputNavBar 
         type={'text'}
